edaa025fd53d974d59414185fcc448fe72443d94
Nick Flynn <nickrflynn@gmail.com>
2017-11-05 22:05:58 -0700

    Consolidated shared styling

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { CoverComponent } from './cover/cover.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CarouselModule.forRoot()
   ],
   declarations: [
     CoverComponent,
